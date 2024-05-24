d98859c3a7cb6f243f9acc1fed85e4f5f79302b8
ulushbey <ulushbey.h@gmail.com>
2024-05-24 20:47:41 +0900

    feat: develop admin related boardArticle GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -13,6 +13,8 @@ export const availablePropertySorts = [
 	'propertyPrice',
 ];
 
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+
 /** IMAGE CONFIGURATION **/
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
