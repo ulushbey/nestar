26e9c8ce5a627121c9775cb4ccb92082cc2a07b3
ulushbey <ulushbey.h@gmail.com>
2024-06-02 05:49:08 +0900

    feat: develop getVisited GraphQL api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -132,3 +132,12 @@ export const lookupFavorite = {
 		as: 'favoriteProperty.memberData',
 	},
 };
+
+export const lookupVisit = {
+	$lookup: {
+		from: 'members',
+		localField: 'visitedProperty.memberId',
+		foreignField: '_id',
+		as: 'visitedProperty.memberData',
+	},
+};
